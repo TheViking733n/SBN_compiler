007f7a560b4aa391bf586433f530b73d1774fa0b
TheViking733n <anant733n@gmail.com>
2023-01-17 13:58:10 +0530

    added readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# SBN Compiler
+Compiles Single Instruction-Set Computer (SIC) assembly code into machine code. The only instruction allowed is SBN (Subtract and Branch if Negative). This compiler was written in Python 3.9.0
+
+## Usage
+```
+python compile.py <input file> <output file>
+```
+
+## Format of input file
+The input file should be a text file with the following format:
+```
--- compile.py
+++ compile.py
@@ -1,13 +1,31 @@
 import sys
 
 
-def getFile():
+
+
+def getInputFile():
+    """Get the file name from the command line or prompt the user for it."""
     if len(sys.argv) > 1:
         return sys.argv[1]
     return "test.txt"
-    return input("Enter SBN file name: ")
+    return input("Enter SBN file name (with extension): ")
+
+def getOutputFile():
+    """Get the file name from the command line or prompt the user for it."""
+    if len(sys.argv) > 2:
+        return sys.argv[2]
+    return "output.txt"
+    return input("Enter output file name (with extension): ")
 
 
 def main():
-    file = getFile()
-    print("Compiling...", file)
+    inp = getInputFile()
+    out = getOutputFile()
+
+    print("Compiling...", inp)
+
+
+
+
+if __name__ == "__main__":
+    main()
