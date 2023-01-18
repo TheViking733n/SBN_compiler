9cdc2b0ec0e5010b9bbc1765c1f98ac3fe25a9c3
TheViking733n <anant733n@gmail.com>
2023-01-18 16:28:33 +0530

    added preprocess()

--- compile.py
+++ compile.py
@@ -1,32 +1,64 @@
 import sys
 
 
+INPUT = None
+OUTPUT = None
+VARS = []
+INSTRUCTIONS = []
 
 
 def getInputFile():
+    global INPUT
     """Get the file name from the command line or prompt the user for it."""
     if len(sys.argv) > 1:
-        return sys.argv[1]
-    return "test.txt"
-    return input("Enter SBN file name (with extension): ")
+        filename = sys.argv[1]
+    else:
+        filename = input("Enter input file name (with extension): ")
+    try:
+        INPUT = open(filename, "r")
+    except FileNotFoundError:
+        print("File not found.")
+        sys.exit()
+
 
 def getOutputFile():
+    global OUTPUT
     """Get the file name from the command line or prompt the user for it."""
     if len(sys.argv) > 2:
-        return sys.argv[2]
-    return "output.txt"
-    return input("Enter output file name (with extension): ")
+        filename = sys.argv[2]
+    else:
+        filename = input("Enter output file name (with extension): ")
+    try:
+        OUTPUT = open(filename, "w")
+    except FileNotFoundError:
+        print("File not found.")
+        sys.exit()
+
+
+def preprocess():
+    """Preprocess the input file."""
+    flag = 1
+    for row in INPUT.readlines():
+        row = row.split('//')[0].strip(' \t').upper()
+        if row.startswith("SBN"):
+            INSTRUCTIONS.append(row)
+            flag = 0
+        elif flag:
+            VARS.append(row)
+    INPUT.close()
 
 
 
 
+
+def compile():
+    
 
 
 def main():
-    inp = getInputFile()
-    out = getOutputFile()
+    getInputFile()
+    getOutputFile()
 
-    print("Compiling...", inp)
 
 
 
